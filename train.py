478492f7065ed2304570ef9ca1147270c317986d
Steve Korshakov <steve@korshakov.com>
2024-02-01 23:31:46 +0000

    feat: using full dataset

--- train.py
+++ train.py
@@ -55,8 +55,8 @@ def main():
 
     # Prepare dataset
     accelerator.print("Loading dataset...")
-    # train_files = glob("external_datasets/libritts-r-clean-100/*/*.wav") + glob("external_datasets/libritts-r-clean-360/*/*.wav") + glob("external_datasets/libritts-r-other-500/*/*.wav")
-    train_files = glob("external_datasets/libritts-r/dev-clean/*/*/*.wav") + glob("external_datasets/libritts-r/dev-other/*/*/*.wav")
+    train_files = glob("external_datasets/libritts-r-clean-100/*/*.wav") + glob("external_datasets/libritts-r-clean-360/*/*.wav") + glob("external_datasets/libritts-r-other-500/*/*.wav")
+    # train_files = glob("external_datasets/libritts-r/dev-clean/*/*/*.wav") + glob("external_datasets/libritts-r/dev-other/*/*/*.wav")
     test_files = glob("external_datasets/libritts-r/test-clean/*/*/*.wav") + glob("external_datasets/libritts-r/test-other/*/*/*.wav")
     train_files.sort() # For reproducibility
     test_files.sort()
